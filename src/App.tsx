dc45d731b4cf0a1eb98386ba5f801f75c2b38d4e
seveneightfive <35607452+seveneightfive@users.noreply.github.com>
2025-07-22 05:03:13 -0500

    Fix scroll to top on navigation and slider dynamic links

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ScrollToTop } from './components/ScrollToTop'
 import { HomePage } from './pages/HomePage'
 import { EventsDirectoryPage } from './pages/EventsDirectoryPage'
 import { ArtistsDirectoryPage } from './pages/ArtistsDirectoryPage'
@@ -13,6 +14,7 @@ import { DashboardPage } from './pages/DashboardPage'
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
